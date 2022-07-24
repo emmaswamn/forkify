49452f64ae9666ea0296eb5a6e8298434e28f190
Emma Han <3547964439@qq.com>
2022-07-24 19:12:32 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL='https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY ='123caf6c-c1cb-414f-b365-15748310a355';
+export const MODAL_CLOSE_SEC =2.5;
